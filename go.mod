aa5b6e5ca7ddc0fdfb41c0c82f11a19e4d7329b1
Kilari Teja <kilariteja9@gmail.com>
2024-07-31 16:31:32 +0530

    Add Picker component

--- go.mod
+++ go.mod
@@ -3,27 +3,52 @@ module github.com/ksdme/mail
 go 1.22.5
 
 require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/bubbletea v0.26.6 // indirect
+	github.com/charmbracelet/lipgloss v0.12.1 // indirect
+	github.com/charmbracelet/x/ansi v0.1.4 // indirect
+	github.com/charmbracelet/x/input v0.1.0 // indirect
+	github.com/charmbracelet/x/term v0.1.1 // indirect
+	github.com/charmbracelet/x/windows v0.1.0 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
 	github.com/emersion/go-sasl v0.0.0-20231106173351-e73c9f7bad43 // indirect
 	github.com/emersion/go-smtp v0.21.3 // indirect
+	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
 	github.com/fatih/color v1.16.0 // indirect
+	github.com/gliderlabs/ssh v0.3.7 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/hashicorp/golang-lru/v2 v2.0.7 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/k3a/html2text v1.2.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sahilm/fuzzy v0.1.1-0.20230530133925-c48e322e2a8f // indirect
 	github.com/tmthrgd/go-hex v0.0.0-20190904060850-447a3041c3bc // indirect
 	github.com/uptrace/bun v1.2.1 // indirect
 	github.com/uptrace/bun/dialect/sqlitedialect v1.2.1 // indirect
 	github.com/uptrace/bun/extra/bundebug v1.2.1 // indirect
 	github.com/vmihailenco/msgpack/v5 v5.4.1 // indirect
 	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
+	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
+	golang.org/x/crypto v0.17.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	modernc.org/gc/v3 v3.0.0-20240304020402-f0dba7c97c2b // indirect
 	modernc.org/libc v1.49.0 // indirect
 	modernc.org/mathutil v1.6.0 // indirect
---
+++ internal/tui/components/picker/picker.go
@@ -0,0 +1,221 @@
+package picker
+
+import (
+	"fmt"
+	"slices"
+
+	"github.com/charmbracelet/bubbles/key"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+)
+
+// A picker is a component that can be used to show a list of
+// values and have the user pick from them.
+type Model struct {
+	title string
+	items []PickerItem
+
+	// Tracks the cursor and selected values.
+	selected    int
+	highlighted int
+	focused     bool
+
+	Width  int
+	Height int
+	Styles Styles
+	KeyMap KeyMap
+}
+
+func NewModel(title string, items []PickerItem, selected int, width int, height int) Model {
+	return Model{
+		title: title,
+		items: items,
+
+		focused:  false,
+		selected: selected,
+
+		Width:  width,
+		Height: height,
+		Styles: DefaultStyles(),
+		KeyMap: DefaultKeyMap(),
+	}
+}
+
+// Handle the focused-ness of the component.
+func (m *Model) Focus() {
+	m.focused = true
+}
+
+func (m *Model) Blur() {
+	m.focused = false
+}
+
+func (m Model) IsFocused() bool {
+	return m.focused
+}
+
+func (m Model) HasItems() bool {
+	return len(m.items) > 0
+}
+
+func (m Model) Init() tea.Cmd {
+	return nil
+}
+
+func (m Model) Update(msg tea.Msg) (Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, m.KeyMap.GoToTop):
+			if m.HasItems() {
+				m.highlighted = m.items[0].Value
+			}
+		case key.Matches(msg, m.KeyMap.GoToLast):
+			if m.HasItems() {
+				m.highlighted = m.items[len(m.items)-1].Value
+			}
+		case key.Matches(msg, m.KeyMap.Up):
+			if m.HasItems() {
+				index := slices.IndexFunc(m.items, func(item PickerItem) bool {
+					return item.Value == m.highlighted
+				})
+				m.highlighted = m.items[max(index-1, 0)].Value
+			}
+		case key.Matches(msg, m.KeyMap.Down):
+			if m.HasItems() {
+				index := slices.IndexFunc(m.items, func(item PickerItem) bool {
+					return item.Value == m.highlighted
+				})
+				m.highlighted = m.items[min(index+1, len(m.items)-1)].Value
+			}
+		case key.Matches(msg, m.KeyMap.Select):
+			// TODO: We should push an command.
+			m.selected = m.highlighted
+		}
+	}
+
+	return m, nil
+}
+
+func (m Model) View() string {
+	lines := []string{}
+
+	// Add a title to the widget.
+	height := m.Height
+	if m.title != "" {
+		title := m.Styles.Title.Render(m.title)
+		lines = append(lines, title)
+		height -= lipgloss.Height(title)
+	}
+
+	// Keep the highlighted line visible.
+	var items []PickerItem
+	location := slices.IndexFunc(m.items, func(item PickerItem) bool {
+		return item.Value == m.highlighted
+	})
+	if location > height-1 {
+		items = m.items[location-height+1 : location+1]
+	} else {
+		items = m.items[:height]
+	}
+
+	for _, item := range items {
+		legend := " "
+		if item.Value == m.selected {
+			legend = "┃"
+		}
+		label := fmt.Sprintf("%s %s", legend, item.Label)
+
+		// If the text overflows, trim it, add ellipsis.
+		total := len(label) + len(item.Badge)
+		if total > m.Width {
+			label = label[:m.Width-total-6] + "..."
+		}
+
+		line := ""
+		if item.Value == m.selected {
+			line = m.Styles.Selected.Render(label)
+		} else if item.Value == m.highlighted {
+			line = m.Styles.Highlighted.Render(label)
+		} else {
+			line = m.Styles.Regular.Render(label)
+		}
+
+		if len(item.Badge) != 0 {
+			badge := m.Styles.Badge.Render(item.Badge)
+			space := m.Width - lipgloss.Width(line) - lipgloss.Width(badge)
+			line = lipgloss.JoinHorizontal(
+				lipgloss.Bottom,
+				line,
+				lipgloss.NewStyle().PaddingRight(space).Render(" "),
+				badge,
+			)
+		}
+
+		lines = append(lines, line)
+	}
+
+	return lipgloss.JoinVertical(lipgloss.Top, lines...)
+}
+
+type Styles struct {
+	Title       lipgloss.Style
+	Badge       lipgloss.Style
+	Regular     lipgloss.Style
+	Selected    lipgloss.Style
+	Highlighted lipgloss.Style
+}
+
+func DefaultStyles() Styles {
+	return Styles{
+		Title:       lipgloss.NewStyle().PaddingLeft(2).Height(2).Foreground(lipgloss.Color("244")),
+		Badge:       lipgloss.NewStyle().Foreground(lipgloss.Color("244")),
+		Regular:     lipgloss.NewStyle(),
+		Selected:    lipgloss.NewStyle().Foreground(lipgloss.Color("212")).Bold(true),
+		Highlighted: lipgloss.NewStyle().Foreground(lipgloss.Color("99")),
+	}
+}
+
+type KeyMap struct {
+	GoToTop  key.Binding
+	GoToLast key.Binding
+
+	Up   key.Binding
+	Down key.Binding
+
+	Select key.Binding
+}
+
+func (k KeyMap) ShortHelp() []key.Binding {
+	return []key.Binding{k.Up, k.Down, k.Select}
+}
+
+func (k KeyMap) FullHelp() [][]key.Binding {
+	return [][]key.Binding{
+		{k.Up, k.Down, k.Select},
+		{k.GoToTop, k.GoToLast},
+	}
+}
+
+func DefaultKeyMap() KeyMap {
+	return KeyMap{
+		GoToTop:  key.NewBinding(key.WithKeys("g"), key.WithHelp("g", "first")),
+		GoToLast: key.NewBinding(key.WithKeys("G"), key.WithHelp("G", "last")),
+
+		Up:   key.NewBinding(key.WithKeys("k", "up", "ctrl+p"), key.WithHelp("k", "up")),
+		Down: key.NewBinding(key.WithKeys("j", "down", "ctrl+n"), key.WithHelp("j", "down")),
+
+		Select: key.NewBinding(key.WithKeys("enter"), key.WithHelp("enter", "select")),
+	}
+}
+
+// Represents an item in the picker list.
+type PickerItem struct {
+	Label string
+	Value int
+	Badge string
+}
+
+func (item *PickerItem) FilterValue() string {
+	return item.Label
+}
