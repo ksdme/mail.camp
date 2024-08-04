dd00629f0b8f29ddf95e463b20eccbfb59b27034
Kilari Teja <kilariteja9@gmail.com>
2024-08-05 02:06:33 +0530

    Add CleanupMails

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,16 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"log"
 	"log/slog"
+	"time"
 
 	"github.com/emersion/go-smtp"
 	"github.com/ksdme/mail/internal/backend"
 	"github.com/ksdme/mail/internal/config"
+	"github.com/ksdme/mail/internal/models"
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/uptrace/bun"
 	"github.com/uptrace/bun/dialect/sqlitedialect"
@@ -24,11 +27,27 @@ func main() {
 	}
 	db := bun.NewDB(sqldb, sqlitedialect.New())
 
+	// SMTP Server
 	s := smtp.NewServer(backend.NewBackend(db))
 	s.Addr = "127.0.0.1:1025"
 	s.Domain = "localhost"
 	s.AllowInsecureAuth = true
+	go runMailServer(s)
 
-	log.Println("Starting SMTP server at", s.Addr)
-	log.Fatal(s.ListenAndServe())
+	// SSH Server
+
+	// Background Workers
+	go runCleanupWorker(db)
+}
+
+func runMailServer(s *smtp.Server) {
+	slog.Info("starting smtp server", "at", s.Addr)
+	slog.Error("failed serving smtp server", "err", s.ListenAndServe())
+}
+
+func runCleanupWorker(db *bun.DB) {
+	for {
+		models.CleanupMails(context.Background(), db)
+		time.Sleep(time.Hour)
+	}
 }
--- internal/models/mail.go
+++ internal/models/mail.go
@@ -1,6 +1,12 @@
 package models
 
-import "time"
+import (
+	"context"
+	"log/slog"
+	"time"
+
+	"github.com/uptrace/bun"
+)
 
 // TODO: Add CreatedAt, UpdatedAt time.
 type Mail struct {
@@ -18,4 +24,23 @@ type Mail struct {
 	Mailbox   *Mailbox `bun:"rel:belongs-to,join:mailbox_id=id"`
 
 	CreatedAt time.Time `bun:",nullzero,notnull,default:current_timestamp"`
+}
+
+// A method that will clean up stale emails.
+func CleanupMails(ctx context.Context, db *bun.DB) error {
+	slog.Info("cleaning up stale mails")
+
+	// Delete all mails that are older than 48 hours.
+	results, err := db.NewDelete().
+		Model(&Mail{}).
+		Where("created_at <= ?", time.Now().Add(-48*time.Hour)).
+		Exec(ctx)
+	if err != nil {
+		slog.Debug("could not clean up stale mails", "err", err)
+		return nil
+	}
+
+	rows, _ := results.RowsAffected()
+	slog.Debug("cleaned up stale mails", "count", rows)
+	return nil
 }
